bb514db6eee7ce30029b2bd0f6370695c797a59f
sweetpoisonxx <avilsu111@gmail.com>
2023-03-06 01:24:36 +0530

    changes added

--- src/constants/images.js
+++ src/constants/images.js
@@ -1,7 +1,45 @@
+<<<<<<< Updated upstream
 export { default as logo } from '../assets/images/yo.png';
 export { default as tmslLogo } from '../assets/images/tmsl-logo.png';
 export { default as eventImg } from '../assets/images/anakhronos/img-3.jpg';
 export { default as heroImg } from '../assets/images/hero_bg.jpg';
+=======
+export { default as logo } from "../assets/images/yo.png";
+export { default as tmslLogo } from "../assets/images/tmsl-logo.png";
+export { default as eventImg } from "../assets/images/anakhronos/img-3.jpg";
+export { default as eventImgBg } from "../assets/images/events_bg.jpg";
+
+export { default as heroImg } from "../assets/images/hero_bg.jpg";
+
+export { default as gallimg1 } from "../assets/images/gallery/GALLERY1.jpg";
+export { default as gallimg2 } from "../assets/images/gallery/GALLERY2.jpg";
+export { default as gallimg3 } from "../assets/images/gallery/GALLERY3.jpg";
+export { default as gallimg4 } from "../assets/images/gallery/GALLERY4.jpg";
+export { default as gallimg5 } from "../assets/images/gallery/GALLERY5.jpg";
+export { default as gallimg6 } from "../assets/images/gallery/GALLERY6.jpg";
+export { default as gallimg7 } from "../assets/images/gallery/GALLERY7.JPG";
+export { default as gallimg8 } from "../assets/images/gallery/GALLERY8.JPG";
+export { default as gallimg9 } from "../assets/images/gallery/GALLERY9.JPG";
+export { default as gallimg10 } from "../assets/images/gallery/GALLERY10.jpg";
+export { default as gallimg11 } from "../assets/images/gallery/GALLERY11.jpg";
+export { default as gallimg12 } from "../assets/images/gallery/GALLERY12.jpg";
+export { default as gallimg13 } from "../assets/images/gallery/GALLERY13.jpg";
+export { default as gallimg14 } from "../assets/images/gallery/gallery_anakhronos.jpg";
+export { default as gallimg15 } from "../assets/images/gallery/gallery_anakhronos1.JPG";
+export { default as gallimg16 } from "../assets/images/gallery/gallery_anakhronos2.jpg";
+export { default as gallimg17 } from "../assets/images/gallery/gallery_fresher.jpg";
+export { default as gallimg18 } from "../assets/images/gallery/gallery_fresher1.JPG";
+export { default as gallimg19 } from "../assets/images/gallery/gallery_fresher2.jpeg";
+export { default as gallimg20 } from "../assets/images/gallery/gallery_fresher3.jpeg";
+export { default as gallimg21 } from "../assets/images/gallery/gallery_fresher4.jpeg";
+export { default as gallimg22 } from "../assets/images/gallery/gallery_fresher5.JPG";
+export { default as gallimg23 } from "../assets/images/gallery/gallery_fresher6.jpeg";
+export { default as gallimg24 } from "../assets/images/gallery/gallery_fresher7.jpeg";
+export { default as gallimg26 } from "../assets/images/gallery/gallery_fresher8.jpeg";
+export { default as gallimg25 } from "../assets/images/gallery/gallery_fresher9.JPG";
+export { default as gallimg27 } from "../assets/images/gallery/GUITAR1.jpg";
+export { default as arrow } from "../assets/images/gallery/arrow.png";
+>>>>>>> Stashed changes
 
 export { default as facebookLogo } from '../assets/images/icons8-facebook.svg';
 export { default as instagramLogo } from '../assets/images/icons8-instagram(1).svg';
--- src/data/events/anakhronos.js
+++ src/data/events/anakhronos.js
@@ -1,58 +1,58 @@
 import { eventImg } from '../../constants/images';
-
+import { gallimg2,gallimg3,gallimg4,gallimg8,gallimg19,gallimg10,gallimg14 } from '../../constants/images';
 export const anakhronosCompetetions = [
   {
     name: 'Sur Tarang',
     description: 'Solo Singing',
-    image: eventImg,
+    image: gallimg4,
     link: '',
   },
   {
     name: 'Step Up',
     description: 'Solo dance',
-    image: eventImg,
+    image: gallimg19,
     link: '',
   },
   {
     name: '2 Mins To Fame',
     description: 'Show case ur talent in 2 mins',
-    image: eventImg,
+    image: gallimg14,
     link: '',
   },
   {
     name: 'Club Feet',
     description: 'The group dance',
-    image: eventImg,
+    image: gallimg3,
     link: '',
   },
   {
     name: 'Band Box',
     description: 'The battle of bands',
-    image: eventImg,
+    image: gallimg8,
     link: '',
   },
   {
     name: 'Panache',
     description: 'Fashion show',
-    image: eventImg,
+    image: gallimg2,
     link: '',
   },
   {
     name: 'Impression',
     description: 'Photography competition',
-    image: eventImg,
+    image: gallimg10,
     link: '',
   },
   {
     name: 'Short Film Making Competiton',
     description: 'Make short films',
-    image: eventImg,
+    image: gallimg10,
     link: '',
   },
   {
     name: 'ART AND CRAFT',
     description: 'Art and craft competition',
-    image: eventImg,
+    image: gallimg10,
     link: '',
   },
 ];
