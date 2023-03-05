c6968543ed57fd1b00d5481e50bda01f46c64648
sweetpoisonxx <avilsu111@gmail.com>
2023-03-05 23:35:21 +0530

    footer added

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { Navbar } from './components';
 import { Hero, About, Events } from './sections';
 
+import Footer from './sections/Footer/Footer';
 import './App.scss';
 
 const App = () => {
@@ -11,6 +12,7 @@ const App = () => {
       <Hero />
       <About />
       <Events />
+      <Footer/>
     </div>
   );
 };
