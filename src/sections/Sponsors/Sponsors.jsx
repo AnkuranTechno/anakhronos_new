8b1cfcf0cfb8298ddd75d70b17440bfc1d9061bf
sweetpoisonxx <avilsu111@gmail.com>
2023-03-06 02:54:21 +0530

    changes added

--- src/sections/Sponsors/Sponsors.jsx
+++ src/sections/Sponsors/Sponsors.jsx
@@ -30,7 +30,7 @@ import './Sponsors.scss';
 
 const Sponsors = () => {
   return (
-    <div className="app__section sponsors" id="sponsors">
+    <div className="app__section sponsors bgcolour_sponsors" id="sponsors">
       <div className="app__section-container sponsors-container">
         <h1>Our sponsors</h1>
         <div className="sponsors-slideshow">
