6ca32df9cf98f09d3ed421e569c72b98e9db1686
Pip coder <73426684+Sreetama2001@users.noreply.github.com>
2023-03-06 10:56:28 +0530

    Update About.jsx

--- src/sections/About/About.jsx
+++ src/sections/About/About.jsx
@@ -10,13 +10,7 @@ const About = () => {
       <div className='about_wrapper'>
         <div className='about_1'>
         <h1>About The Event</h1>
-        <p>Techno Main Salt Lake, was established in the year of 2001. In the test of time, it has
-          stood tall and thrives to be
-          better than what it was yesterday. It is a place where
-          everyone is motivated to be the best versions of themselves
-          and do what they can do their best. A college where everyone believes that practice makes a human perfect,
-          our efforts always pay off
-          and that dreams do come true</p>
+        <p> Annual Cultural Fest of Techno India Salt Lake oragnized by team.</p>
           </div>
           <div className='about_2'>
             <h2>Where</h2>
