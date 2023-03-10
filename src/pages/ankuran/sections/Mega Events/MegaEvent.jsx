3b27ffe241c5b2b4d9e4b7bcb9531fc2a978903e
ANKUSH Banerjeee <banerjeeankush184@gmail.com>
2023-03-11 02:42:33 +0530

    card created

--- src/pages/ankuran/sections/Mega Events/MegaEvent.jsx
+++ src/pages/ankuran/sections/Mega Events/MegaEvent.jsx
@@ -4,7 +4,7 @@ import MegaCard from './MegaEventCard';
 const MegaEvent = () => {
     return (
         <div>
-            <MegaCard img='https://user-images.githubusercontent.com/102872077/223775333-f7335900-500c-4d2d-83bc-f834097bace3.png' desc="kfdlkanlfknlakdnflkandsflknalnlkkkkkkkkkkkkkkkkkkkkkkkkkk" eventname='ankush'/>
+            <MegaCard />
         </div>
     );
 }
--- src/pages/ankuran/sections/Mega Events/MegaEventCard.jsx
+++ src/pages/ankuran/sections/Mega Events/MegaEventCard.jsx
@@ -3,22 +3,38 @@ import "./MegaEventCard.css"
 const MegaCard = (props) => {
     console.log({props});
     return (
-        <div className='events'>
-           <div id='event-card'>
-            <div className='red'>
-                    <img src={props.img} alt='event_image' />
-            </div>
-            <div className='green'>
-                <div className='title'>
-                    <h2>{props.eventname}</h2>
-                 
+        <><div className='events'>
+            <div id='event-card'>
+                <div className='red'>
+                    <img src="https://raw.githubusercontent.com/Ankush109/anakhronos_new/master/src/assets/anakhronos/images/Prarambh.png" alt='event_image' />
                 </div>
-                  <div className='desc'>
-                    <p>{props.desc}</p>
+                <div className='green'>
+                    <div className='title'>
+                        <h2>Prarambh</h2>
+
                     </div>
+                    <div className='desc'>
+                        <p>about the event some imp details</p>
+                    </div>
+                </div>
             </div>
-        </div>
-        </div>
+
+        </div><div className='events2'>
+                <div id='event-card'>
+                    <div className='red'>
+                        <img src="https://raw.githubusercontent.com/Ankush109/anakhronos_new/master/src/assets/anakhronos/images/Prarambh.png" alt='event_image' />
+                    </div>
+                    <div className='green'>
+                        <div className='title'>
+                            <h2>Prarambh</h2>
+
+                        </div>
+                        <div className='desc'>
+                            <p>about the event some imp details</p>
+                        </div>
+                    </div>
+                </div>
+            </div></>
     );
 }
 
