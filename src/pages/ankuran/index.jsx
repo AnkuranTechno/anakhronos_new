365a840bcd48c794ab877762a6f83ebbc28d0070
Sreetama <fool100abc@gmail.com>
2023-03-12 22:37:31 +0530

    changes

--- src/pages/ankuran/index.jsx
+++ src/pages/ankuran/index.jsx
@@ -1,9 +1,9 @@
 import React from 'react'
-import { Navbar, Team } from './sections'
+import { Team } from './sections'
+import { Navbar } from '../../components';
 import { navLinks } from "../../data/events/anakhronos";
 import {tmslLogo } from "../../constants/images";
 import Megaevent from './sections/Mega Events/MegaEventCard';
-
 const Ankuran = () => {
   return (
     <div className='app'>
