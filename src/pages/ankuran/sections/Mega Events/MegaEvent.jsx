9e78335a0f8a386970137c027a76b7566e0fa5cd
ANKUSH Banerjeee <banerjeeankush184@gmail.com>
2023-03-25 02:44:47 +0530

    fixed some css

--- src/pages/ankuran/sections/Mega Events/MegaEvent.jsx
+++ src/pages/ankuran/sections/Mega Events/MegaEvent.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import MegaCard from './MegaEventCard';
-
+import './MegaEventCard.css'
 const MegaEvent = () => {
     return (
         <div className='megaevents_wrapper'>
