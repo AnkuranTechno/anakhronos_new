acc50623f2d81f7fec99de83651da488f5349980
Akash <ag308669@gmail.com>
2023-04-04 01:39:10 +0530

    Added mega events heading

--- src/pages/ankuran/sections/Mega Events/MegaEvent.jsx
+++ src/pages/ankuran/sections/Mega Events/MegaEvent.jsx
@@ -6,24 +6,27 @@ const MegaEvent = () => {
   return (
     <section class="mega-events-section timeline bgcolour">
       <div class="section-container">
-        <MegaCard
-          title="Prarambha"
-          date="25 MAY 2023"
-          desc="Lorem ipsum dolor sit amet, consectetur adipisicing elit. Maxime ipsa
+        <h1 className="mega-events-heading">Mega Events</h1>
+        <div className="mega-events-cards-container">
+          <MegaCard
+            title="Prarambha"
+            date="25 MAY 2023"
+            desc="Lorem ipsum dolor sit amet, consectetur adipisicing elit. Maxime ipsa
           ratione omnis alias cupiditate saepe atque totam aperiam sed nulla
           voluptatem recusandae dolor, nostrum excepturi amet in dolores. Alias,
           ullam."
-          link="freshers"
-        />
-        <MegaCard
-          title="Anakhronos"
-          date="25 MAY 2023"
-          desc="Lorem ipsum dolor sit amet, consectetur adipisicing elit. Maxime ipsa
+            link="freshers"
+          />
+          <MegaCard
+            title="Anakhronos"
+            date="25 MAY 2023"
+            desc="Lorem ipsum dolor sit amet, consectetur adipisicing elit. Maxime ipsa
           ratione omnis alias cupiditate saepe atque totam aperiam sed nulla
           voluptatem recusandae dolor, nostrum excepturi amet in dolores. Alias,
           ullam."
-          link="anakhronos"
-        />
+            link="anakhronos"
+          />
+        </div>
       </div>
     </section>
   );
