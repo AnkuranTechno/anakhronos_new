564fb5477da53a69d79c1df377851d45da1ab3d3
Akash <ag308669@gmail.com>
2023-04-08 18:20:14 +0530

    Modified anakhronos logo

--- src/pages/anakhronos/sections/Hero/Hero.jsx
+++ src/pages/anakhronos/sections/Hero/Hero.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Button } from '../../../../components';
-import { logo } from '../../../../constants/images';
+import { logo, anakhronosLogoText } from '../../../../constants/images';
 import './Hero.scss';
 
 const Hero = () => {
@@ -8,7 +8,12 @@ const Hero = () => {
     <div className="anakhronos__hero" id="hero">
       <div className="anakhronos__hero__container">
         <div className="anakhronos__hero__img-container">
-          <img className="anakhronos__hero__img" src={logo} alt="yo" />
+          <img className="anakhronos__hero__img yo" src={logo} alt="yo" />
+          <img
+            className="anakhronos__hero__img"
+            src={anakhronosLogoText}
+            alt="anakhronos"
+          />
         </div>
         <div className="anakhronos__hero__content">
           <h1 className="anakhronos__hero__heading">Anakhronos</h1>
