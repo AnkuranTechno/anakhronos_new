e2946dcacaf654ec01fa9a73042214d88dcca94b
Asif Ali <asif.a.7277@gmail.com>
2023-04-11 22:11:17 +0530

    changed the image and styl

--- src/pages/ankuran/sections/Mega Events/MegaEventCard.jsx
+++ src/pages/ankuran/sections/Mega Events/MegaEventCard.jsx
@@ -1,13 +1,17 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { anakhImg1 } from '../../../../constants/anakhronosImg';
 
-const MegaCard = ({ title, date, desc, link }) => {
+const MegaCard = ({ title, date, desc, link ,img}) => {
   return (
     <div class="timeline-item">
       <div class="timeline-img"></div>
 
       <div class="timeline-content timeline-card js--fadeInRight">
         <div class="timeline-img-header">
+          <div  className='imagess'>
+           <img src={img} alt=''/>
+          </div>
           <h2>{title}</h2>
         </div>
         <div class="date">{date}</div>
