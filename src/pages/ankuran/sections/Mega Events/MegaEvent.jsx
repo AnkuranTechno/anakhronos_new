b5ce09dcfc40858fe5c423a5f6ce68b4b2bd10ef
Akash <ag308669@gmail.com>
2023-04-12 22:20:50 +0530

    Fixed mega events bg

--- src/pages/ankuran/sections/Mega Events/MegaEvent.jsx
+++ src/pages/ankuran/sections/Mega Events/MegaEvent.jsx
@@ -5,7 +5,7 @@ import { Anakhronos, FreshersMega } from '../../../../constants/images';
 
 const MegaEvent = () => {
   return (
-    <section class="mega-events-section timeline bgcolour">
+    <section class="mega-events-section timeline gradient-bg">
       <div class="section-container">
         <h1 className="mega-events-heading">Mega Events</h1>
         <div className="mega-events-cards-container">
